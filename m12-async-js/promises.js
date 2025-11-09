4cd2855352856848592a40351a979ad3a7ad817d
Rishi <rishikm215@gmail.com>
2025-11-09 16:04:44 +0530

    final commit

--- m12-async-js/promises.js
+++ m12-async-js/promises.js
@@ -13,3 +13,31 @@ function asyncTask(data) {
 }
 
 asyncTask("X").then(res => console.log(res)).catch(err => console.error(err));
+
+// let p1 = new Promise((resolve, reject)=>{
+//     console.log("this is a promise")
+//     // resolve({
+//     //     name:"tushar"
+//     // })
+//     reject("something went wrong..!!")
+// })
+
+p1.then((data)=>{
+    console.log(data)
+})
+
+p1.catch((err)=>{
+    console.log(err)
+})
+
+
+// p1.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+
+
+
