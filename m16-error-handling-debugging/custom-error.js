45e5e64147c3796494acc25f2f5dbb17f0bdb1b4
Madhav Singh <138430221+madhav-kr-singh@users.noreply.github.com>
2025-11-09 14:25:50 +0530

    Add examples of throwing custom and built-in error
    Demonstrate various ways to throw errors in JavaScript, including custom errors and built-in error types.

--- m16-error-handling-debugging/custom-error.js
+++ m16-error-handling-debugging/custom-error.js
@@ -4,3 +4,48 @@ function MyError(msg) {
 }
 MyError.prototype = Error.prototype;
 throw new MyError("Custom error occurred!");
+
+
+
+
+
+// throw :  it is used to throw a custom error message 
+
+try {
+  let age = -5;
+  if (age < 0) {
+    throw new Error("Age cannot be negative");
+  }
+  console.log("Valid age:", age);
+} 
+catch (err) {
+  console.error("Custom error caught:", err.message);
+}
+finally {
+  console.log("Validation completed");
+}
+
+
+
+
+// throw error can be 
+
+// 1. String
+throw "Something went wrong!";
+
+// 2. Number
+throw 1021;
+
+// 3. Boolean
+throw true;
+
+// 4. Object
+throw { name: "CustomError", message: "Error occurred" };
+
+// 5. Error Object (RECOMMENDED)
+throw new Error("This is a proper error");
+
+// 6. Custom Error Object
+throw new SyntaxError("Invalid syntax");
+throw new TypeError("Wrong type");
+throw new RangeError("Value out of range");
